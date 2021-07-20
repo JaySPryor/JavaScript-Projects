71e1458c534727c22c0cd731a5e2e184f4752481
JayP <86741921+JaySPryor@users.noreply.github.com>
2021-07-19 21:03:11 -0600

    created Project2 folder

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,17 +1,17 @@
 
-var A = "\"This\""+" string is "+"blue";//demonstrates the escape character &
-                                    //concatenates a string with + character
-    A = A.fontcolor("blue");        //sets var A to blue font
+var A = "\"This\""+" string is "+"blue";//Demonstrates the escape character &
+                                    // concatenates a string with + character
+    A = A.fontcolor("blue");        //Sets var A to blue font
     B = " ...or is it?";
-    B = B.fontcolor("Red");         //sets var B to a red font
-document.write(A+B);                //writes variables A & B to doc
+    B = B.fontcolor("Red");         //Sets var B to a red font
+document.write(A+B);                //Writes variables A & B to doc
 
-window.alert("this is a window");   //this is the popup window
-
+function myFunk() {
+    window.alert("this is a window");//This is the popup window that is generated "onload"
+}                                  // of page.
 // button 
 function My_First_Function() {              //Defines a function and names it
     var str = "This text is green!";        //Defines a variable and gives it a str value
     var result = str.fontcolor("green");    //Uses fontcolor method on str variable
-    document.getElementById("Green_Text").innerHTML = result;//puts value  of result into 
-                                            //html element with "Green_Text" id
-}
+    document.getElementById("Green_Text").innerHTML = result;//Puts value  of result into
+}                                           // html element with "Green_Text" id
---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+//The function for the clickable text
+function someFunction() {//Name of function
+    var reply = "wait!";//Name of variable
+    reply += " ...I wasn't ready.";//Used to concatenate the two variables
+    document.getElementById("Concatenate").innerHTML = reply;
+}
