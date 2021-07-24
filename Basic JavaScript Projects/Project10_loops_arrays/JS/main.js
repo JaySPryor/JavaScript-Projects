8444eb41c5fd05ab96623076307e79b20b3c4e06
JayP <86741921+JaySPryor@users.noreply.github.com>
2021-07-23 21:57:01 -0600

    Loops & Arrays

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,4 +1,4 @@
-
+/********** LOOPS & ARRAYS **********/
 
 //basic while loop
 function Call_Loop() {
@@ -37,3 +37,49 @@ function array_Function() {
     Cat_Picture[3] = "Purring";
     document.getElementById("Array").innerHTML = Cat_Picture[0];
 }
+
+//Const keyword
+function constant_Function() {
+    const Musical_Instrument = {type:"guitar", brand:"Fender", color:"Black"};
+    Musical_Instrument.color = "blue";
+    Musical_Instrument.price = "$900";
+    document.getElementById("Constant").innerHTML = "The color of the " + Musical_Instrument.type + " is now " + Musical_Instrument.color + "!";
+}
+
+//let keyword
+function letKeyword() {
+    var x = 111;
+    document.write(x);
+    {
+        let x = 333;
+        document.write("<br>" + x);
+    }
+    document.write("<br>" + x);
+}
+
+//return statement
+function returnFunc() {
+    return Math.PI;
+}
+document.getElementById("piReturn").innerHTML = returnFunc();
+
+//object demo
+let car = {
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color: "red ",
+    description : function() {
+        return "The car is a " + this.year + this.color + this.make + this.model;
+    }
+}
+document.getElementById("carObject").innerHTML = car.description();
+
+//break & continue statements
+let text = "";
+for (let i = 0; i < 10; i++) {
+    if (i === 3) { continue; }//Skips #3
+    if (i === 8) { break; }//Ends loop after 7
+    text += i + "<br>";
+}
+document.getElementById("breakCont").innerHTML = text;
